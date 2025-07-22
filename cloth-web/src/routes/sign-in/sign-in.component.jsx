c92af50541f0cad75f576a4d16e8bb448e005d13
ZENG <74520775+hashtag47@users.noreply.github.com>
2025-07-22 15:33:53 +0100

    Add google redirect function but failed as instruction and further correction

--- cloth-web/src/routes/sign-in/sign-in.component.jsx
+++ cloth-web/src/routes/sign-in/sign-in.component.jsx
@@ -1,10 +1,31 @@
 import React from "react";
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
+
 import {
+  auth,
   signInWithGooglePopup,
+  signInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
+  //FIXME: No data returned from google redirected page
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     try {
+  //       const response = await getRedirectResult(auth);
+  //       if (response) {
+  //         const { user } = response;
+  //         await createUserDocumentFromAuth(user);
+  //       }
+  //     } catch (error) {
+  //       console.error("Error handling redirect result:", error);
+  //     }
+  //   }
+  //   fetchData();
+  // }, []);
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
@@ -14,6 +35,10 @@ const SignIn = () => {
     <div>
       <h1>I am the signIn page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      {/*TODO:*/}
+      {/* <button onClick={signInWithGoogleRedirect}>
+        Sign In with Google Redirect
+      </button> */}
     </div>
   );
 };
