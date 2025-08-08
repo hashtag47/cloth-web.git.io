5049a90639cd3857040c1acf8559b9d70fd67f7a
ZENG <74520775+hashtag47@users.noreply.github.com>
2025-08-08 15:03:28 +0100

    Add redux-persist in store.js to record in localStorage

--- cloth-web/src/main.jsx
+++ cloth-web/src/main.jsx
@@ -2,27 +2,30 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 import App from "./App.jsx";
 // import { UserProvider } from "./contexts/user.provider.jsx";
 // import { CategoriesProvider } from "./contexts/categories.provider.jsx";
 // import { CartDropdownProvider } from "./contexts/cart-dropdown.provider.jsx";
-import { store } from "./store/store";
+import { store, persistor } from "./store/store";
 
 import "./index.scss";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        {/* <UserProvider> */}
-        {/* <CategoriesProvider> */}
-        {/* <CartDropdownProvider> */}
-        <App />
-        {/* </CartDropdownProvider> */}
-        {/* </CategoriesProvider> */}
-        {/* </UserProvider> */}
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          {/* <UserProvider> */}
+          {/* <CategoriesProvider> */}
+          {/* <CartDropdownProvider> */}
+          <App />
+          {/* </CartDropdownProvider> */}
+          {/* </CategoriesProvider> */}
+          {/* </UserProvider> */}
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
